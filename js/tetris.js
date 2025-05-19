0a4f0da6a3d74a75ebfc4c51f189eac3590bf3b6
Yuma Masuda <182611723+yuma25@users.noreply.github.com>
2025-05-19 15:36:34 +0900

    ピースの色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'purple', 'purple', 'purple', 'purple', 'purple', 'purple', 'purple'
+    'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
